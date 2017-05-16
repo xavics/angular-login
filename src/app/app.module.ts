0faca3a49384a08036c7e331ec2e4e0f0a4b7784
xavics <xacosa@gmail.com>
2017-05-16 13:00:39 +0200

    Added log in reactive form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -8,6 +8,8 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {CoreModule} from "./core/core.module";
 import {AuthenticationComponent} from "./authentication/authentication.component";
 import {Routing} from "./app.routing";
+import {MdInputModule} from '@angular/material';
+import {MdButtonModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,10 @@ import {Routing} from "./app.routing";
     HttpModule,
     Routing,
     BrowserAnimationsModule,
-    CoreModule
+    CoreModule,
+    ReactiveFormsModule,
+    MdInputModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -2,11 +2,26 @@
  * Created by xavi on 5/16/17.
  */
 import {Component} from "@angular/core";
+import {Validators, FormGroup, FormBuilder} from "@angular/forms";
 @Component({
   selector: 'authentication',
   templateUrl: 'authentication.component.html'
 })
 
 export class AuthenticationComponent {
+  public loginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  public submitLogin(): void {
+    console.info("Form: ", this.loginForm.value)
+  }
 }
