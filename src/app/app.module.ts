cbe553cfb494c27812d33360ffec7f88bb582c1f
xavics <xacosa@gmail.com>
2017-05-16 12:15:43 +0200

    User model created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     BrowserModule,
     FormsModule,
     HttpModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+
+  constructor(
+    public id: string,
+    public name: string,
+    public surname: string,
+    public email: string,
+  ) { }
+}
+
